f7802abdb7a05a5c1bdd8da084ab0583dff7eb15
toho36 <toho0036@gmail.com>
2023-05-21 11:02:07 +0200

    259 Http request with feedback

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
 import { useSelector } from 'react-redux';
-import { useEffect } from 'react';
+import { Fragment, useEffect } from 'react';
+import { uiActions } from './store/ui-slice';
+import { useDispatch } from 'react-redux';
 
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
+import Notification from './components/UI/Notification';
+
+let isInitial = true;
 
 function App() {
   const showCart = useSelector((state) => state.ui.cartIsVisible);
   const cart = useSelector((state) => state.cart);
+  const dispatch = useDispatch();
+  const notification = useSelector((state) => state.ui.notification);
 
   useEffect(() => {
-    fetch(
-      'https://react-http-98ba6-default-rtdb.europe-west1.firebasedatabase.app/cart.json',
-      { method: 'PUT', body: JSON.stringify(cart) }
-    );
-  }, [cart]);
+    const sendCartData = async () => {
+      dispatch(
+        uiActions.showNotification({
+          status: 'pending',
+          title: 'Sending...',
+          message: 'sending cart data',
+        })
+      );
+      const response = await fetch(
+        'https://react-http-98ba6-default-rtdb.europe-west1.firebasedatabase.app/cart.json',
+        { method: 'PUT', body: JSON.stringify(cart) }
+      );
+      if (!response.ok) {
+        throw new Error('Sending data failed');
+      }
+      dispatch(
+        uiActions.showNotification({
+          status: 'success',
+          title: 'Success',
+          message: 'sending cart data successfully',
+        })
+      );
+    };
+    if (isInitial) {
+      isInitial = false;
+      return;
+    }
+    sendCartData().catch((error) => {
+      dispatch(
+        uiActions.showNotification({
+          status: 'error',
+          title: 'Error',
+          message: 'sending cart data failed',
+        })
+      );
+    });
+  }, [cart, dispatch]);
   return (
-    <Layout>
-      {showCart && <Cart />}
-      <Products />
-    </Layout>
+    <Fragment>
+      {notification && (
+        <Notification
+          status={notification.status}
+          title={notification.title}
+          message={notification.message}
+        />
+      )}
+      <Layout>
+        {showCart && <Cart />}
+        <Products />
+      </Layout>
+    </Fragment>
   );
 }
 
