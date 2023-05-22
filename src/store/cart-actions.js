5ed1a3ee73901ace2504324ad08867581f50b70e
toho36 <toho0036@gmail.com>
2023-05-22 11:24:29 +0200

    262 fixing when cart is empty
    and sending data only which we want

--- src/store/cart-actions.js
+++ src/store/cart-actions.js
@@ -17,7 +17,12 @@ export const fetchCartData = () => {
 
     try {
       const cartData = await fetchData();
-      dispatch(cartActions.replaceCart(cartData));
+      dispatch(
+        cartActions.replaceCart({
+          items: cartData.items || [],
+          totalQuantity: cartData.totalQuantity,
+        })
+      );
     } catch (error) {
       dispatch(
         uiActions.showNotification({
@@ -42,7 +47,13 @@ export const sendCartData = (cart) => {
     const sendRequest = async () => {
       const response = await fetch(
         'https://react-http-98ba6-default-rtdb.europe-west1.firebasedatabase.app/cart.json',
-        { method: 'PUT', body: JSON.stringify(cart) }
+        {
+          method: 'PUT',
+          body: JSON.stringify({
+            items: cart.items,
+            totalQuantity: cart.totalQuantity,
+          }),
+        }
       );
       if (!response.ok) {
         throw new Error('Sending data failed');
